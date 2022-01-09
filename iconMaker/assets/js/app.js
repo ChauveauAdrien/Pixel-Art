b88617f92bbeb14bdcfadabf61433a6be8e33812
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-01-09 21:14:33 +0100

    version finale avec opti et commentaires pour upgrades

--- iconMaker/assets/js/app.js
+++ iconMaker/assets/js/app.js
@@ -2,106 +2,136 @@
 // Votre code ici
 // -----------------------------------------------------------
 
-function createPixel() {
+// -----------------------FONCTION----------------------------
+// -- Fonction qui redimensionne les grilles
+// -----------------------------------------------------------
+// function resizeGrid() {
+//   let screen = $(".app-screen");
+//   let btn32 = $(".size-btn-32");
+//   let btn64 = $(".size-btn-64");
+//   let info = $(".info");
+//   btn32.click(function () {
+//     let pixels = $(".pixel");
+//     pixels.remove();
+//     info.remove();
+//     let btn32Value = $(this).val();
+//     screen.css({
+//       "display": "grid",
+//       "grid-template-columns": "repeat(32, 2vmin)",
+//       "grid-template-rows": "repeat(32, 2vmin)",
+//     });
+//     createPixel(btn32Value);
+//   });
+//   btn64.click(function () {
+//     let pixels = $(".pixel");
+//     pixels.remove();
+//     info.remove();
+//     let btn64Value = $(this).val();
+//     screen.css({
+//       "display": "grid",
+//       "grid-template-columns": "repeat(64, 1vmin)",
+//       "grid-template-rows": "repeat(64, 1vmin)",
+//     });
+//     createPixel(btn64Value);
+
+//   });
+// }
+function createPixel(size) {
   let pixelDiv = '<div class="pixel"></div>';
-  $(".app-screen").append(pixelDiv.repeat(1024));
+  $(".app-screen").append(pixelDiv.repeat(size * size));
 }
-
-function pickPixel() {
-  let darkestgreen = $(".bg_darkestgreen");
-  let darkgreen = $(".bg_darkgreen");
-  let green = $(".bg_green");
-  let lightgreen = $(".bg_lightgreen");
-
-  // -----------------------------------------------------------
-  // vert foncé
-  // -----------------------------------------------------------
-  darkestgreen.click(function () {
-    let getActiveElmt = document.querySelector(".is-active");
-    getActiveElmt.classList.remove("is-active");
-    darkestgreen.addClass("is-active");
-    let color = darkestgreen.css("backgroundColor");
-    let pixels = $(".pixel");
-    pixels.click(function () {
-      $(this).css("background", color);
-    });
-  });
-  // -----------------------------------------------------------
-  // vert moins foncé
-  // -----------------------------------------------------------
-  darkgreen.click(function () {
-    let getActiveElmt = document.querySelector(".is-active");
-    getActiveElmt.classList.remove("is-active");
-    darkgreen.addClass("is-active");
-    let color = darkgreen.css("backgroundColor");
-    let pixels = $(".pixel");
-    pixels.click(function () {
-      $(this).css("background", color);
-    });
-  });
-  // -----------------------------------------------------------
-  // vert un peu foncé
-  // -----------------------------------------------------------
-  green.click(function () {
-    let getActiveElmt = document.querySelector(".is-active");
-    getActiveElmt.classList.remove("is-active");
-    green.addClass("is-active");
-    let color = green.css("backgroundColor");
-    let pixels = $(".pixel");
-    pixels.click(function () {
-      $(this).css("background", color);
-    });
-  });
-  // -----------------------------------------------------------
-  // vert vraiment beaucoup moins foncé, vert clair ducoup
-  // -----------------------------------------------------------
-  lightgreen.click(function () {
-    let getActiveElmt = document.querySelector(".is-active");
-    getActiveElmt.classList.remove("is-active");
-    lightgreen.addClass("is-active");
-    let color = lightgreen.css("backgroundColor");
-    let pixels = $(".pixel");
-    pixels.click(function () {
-      $(this).css("background", color);
+// -----------------------FONCTION----------------------------
+// -- Fonction qui ajoute des nouvelles couleurs
+// -----------------------------------------------------------
+function addColor() {
+  let addBtn = $(".add");
+  let newColor = $("#colorPicker");
+  let btns = $(".buttons");
+  addBtn.click(function () {
+    let color = newColor.val();
+    btns.prepend('<button id="new-color" class="swatch"></button>');
+    let newBtn = btns.children().first();
+    let activeBtn = document.querySelector(".is-active");
+    activeBtn.classList.remove("is-active");
+    newBtn.addClass("is-active").css("background", color);
+    let btnadd = $(".swatch");
+    btnadd.click(function () {
+      let activeBtn = document.querySelector(".is-active");
+      activeBtn.classList.remove("is-active");
+      $(this).addClass("is-active");
     });
   });
 }
 
+// -----------------------FONCTION----------------------------
+// -- Fonction qui active et desactive les boutons de couleurs
+// -----------------------------------------------------------
+function activateBtn() {
+  let btn = $(".swatch");
+  btn.click(function () {
+    let activeBtn = document.querySelector(".is-active");
+    activeBtn.classList.remove("is-active");
+    $(this).addClass("is-active");
+  });
+}
+
+// -----------------------FONCTION----------------------------
+// -- Fonction qui colore les pixels en fonction du bouton actif
+// -----------------------------------------------------------
+function colorPixel() {
+  let pixel = $(".pixel");
+  pixel.click(function () {
+    let colorActiveBtn = $(".is-active");
+    let color = colorActiveBtn.css("backgroundColor");
+    $(this).css({ background: color, border: "none" });
+  });
+}
+
+// -----------------------FONCTION----------------------------
+// -- Fonction qui gomme le pixel au double click
+// -----------------------------------------------------------
 function erase() {
   let pixel = $(".pixel");
   pixel.dblclick(function () {
-    $(this).css("background", "none");
+    $(this).css({ background: "none", border: "1px solid rgb(221, 221, 221)" });
   });
 }
 
+// -----------------------FONCTION----------------------------
+// -- Fonction qui reset la page
+// -----------------------------------------------------------
 function resetPixel() {
   let resetBtn = $("#reset");
   let pixels = $(".app-screen").children();
   resetBtn.click(function () {
-    pixels.css("background", "none");
+    pixels.css({ background: "none", border: "1px solid rgb(221, 221, 221)" });
   });
 }
-function exportBtn(){
-  let exp = $('#export');
-  exp.click(function(){
-      domtoimage.toJpeg(document.getElementById('content'))
-      .then(function(dataUrl){
-          let link = document.createElement('a');
-          link.download = 'image.jpeg';
-          link.href = dataUrl;
-          link.click();
+
+// -----------------------FONCTION----------------------------
+// -- Fonction qui permet l'export du dessin
+// -----------------------------------------------------------
+function exportBtn() {
+  let exp = $("#export");
+  exp.click(function () {
+    domtoimage
+      .toJpeg(document.getElementById("content"))
+      .then(function (dataUrl) {
+        let link = document.createElement("a");
+        link.download = "image.jpeg";
+        link.href = dataUrl;
+        link.click();
       });
   });
 }
-
-
-
-
+// Fonction ready
 $(function () {
-  createPixel();
-  pickPixel();
+  createPixel(32);
+  // resizeGrid();
+  addColor();
+  activateBtn();
+  colorPixel();
   resetPixel();
   erase();
   exportBtn();
-
 });
